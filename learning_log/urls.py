4ea30c23a9f1bd3e674434175943152a3e7ab2f4
Prathmesh Pathak <pprathamesh1594@gmail.com>
2018-03-19 16:22:36 +0530

    added editing functionality

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -1,14 +1,7 @@
-
-
 from django.conf.urls import include, url
 from django.contrib import admin
-from . import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'learning_log.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include("learning_logs.urls", namespace = "learning_logs"))
+    url(r'', include('learning_logs.urls', namespace='learning_logs')),
 ]
